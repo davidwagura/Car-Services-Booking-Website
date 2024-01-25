2e91b544b0e2546fbb8f28b324c7d58d9482b227
David wanjohi <davidwagura83@gmail.com>
2024-01-25 16:22:18 +0300

    book service details

--- resources/views/pages/bookService.blade.php
+++ resources/views/pages/bookService.blade.php
@@ -8,7 +8,58 @@
 
     </head>
     </body>
+    <style>
+      section {
+        text-align: center;
+      }
+
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        float: center;
+        display: block;
+      }
+      select {
+        padding: auto;
+        margin:10px 10px 10px 10px;
+        padding: 15px 32px; 
+
+      }
+      textarea {
+        height: 7rem;
+        margin:10px 10px 10px 10px;
+      }
+      button {
+        text-align: center;
+        margin:10px 10px 10px 10px;
+        padding: 15px 32px;
+
+      }
+      input {
+        margin:10px 10px 10px 10px;
+        padding: 15px 32px; 
+      }
+    </style>
       <h1>Please fill the form below</h1>   
-      <p>This is an about page</p>   
+       
+        <label for="name">fullname:</label>
+        <input type="text"><br>
+        <label for="services">select service:</label>
+        <select name="services" id="book service">
+        <option value="select">choose a service</option>
+        <option value="oil">Oil Change</option>
+        <option value="engine">Engine Diagnosis</option>
+        <option value="wheel">Tire Rotation</option>
+        <option value="breaks">Break Inspection</option>
+        </select><br>  
+        <label for= "description">please describe the car's condition:</label> <br>
+        <textarea name="description" id="car-details"></textarea>
+        <section>
+        <button>submit</button>
+        </section>
+
     </body>
 </html>
